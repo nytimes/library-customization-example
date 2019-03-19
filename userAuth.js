65a6e70240cfee31e444fa20fe74ae32daf437d9
Isaac White <isaac.white@nytimes.com>
2019-03-19 10:24:25 -0400

    disable user auth, do not set GA user id

--- userAuth.js
+++ userAuth.js
@@ -3,16 +3,12 @@
 const md5 = require('md5')
 const router = require('express-promise-router')()
 
-// @TODO: Remove below code block when ready to publish the demo site
-const defaultAuth = require('../server/userAuth')
-router.use(defaultAuth)
-// END @TODO REMOVE BLOCK
-
 router.use(async (req, res) => {
   req.userInfo = {
     email: 'demo.user@example.com',
     userId: '10',
-    analyticsUserId: md5('10library')
+    // prevents incorrect GA tracking
+    analyticsUserId: false
   }
   
   return 'next'
